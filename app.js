3cae01acd7b79fd50074d20fa8bca2577546ac01
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2023-08-09 15:26:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT||5500
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.get("/home",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/simple_server.py'));
+});
 app.listen(PORT,()=>{
     console.log(`${PORT}`);
 });
